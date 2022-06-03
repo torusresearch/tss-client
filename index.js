dd73b3e92934afd9fd9e1a20c053ab37cd47d932
Leonard <tetratorus@gmail.com>
2022-06-03 17:22:32 +0800

    round 4 verify done

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post("/pubkey/:user", async (req, res) => {
 });
 
 app.post("/round_1", async (req, res) => {
-  let { endpoints, user } = req.body; // TODO: get from db
+  let { endpoints, index, user } = req.body; // TODO: get from db
   let gamma_i = tss.random_bigint();
   let [com, blind_factor, g_gamma_i] = tss.phase_1_broadcast(gamma_i);
   let k_i = tss.random_bigint();
@@ -199,20 +199,68 @@ app.post("/round_2_Alphas", async (req, res) => {
 });
 
 app.post("/round_3_DeltaInv", async (req, res) => {
-  let { user, index, parties } = req.body
-  let deltas = []
+  let { user, parties } = req.body;
+  let deltas = [];
   for (let i = 0; i < parties.length; i++) {
-    let party = parties[i]
-    deltas.push(await db.get(`node-${party}:${user}:delta`))
+    let party = parties[i];
+    deltas.push(await db.get(`node-${party}:${user}:delta`));
   }
   let deltaInv = tss.phase_3_deltaInv(deltas);
-  await db.set(`${nodeKey}:delta_inv`, deltaInv)
-  res.sendStatus(200)
-})
+  await db.set(`${nodeKey}:${user}:delta_inv`, deltaInv);
+  res.sendStatus(200);
+});
 
-// app.post("/round_4_Di")
+app.post("/round_4_Di", async (req, res) => {
+  let { user, index, endpoints } = req.body;
+  let Di = await db.get(`${nodeKey}:${user}:g_gamma_i`);
+  let Diblind = await db.get(`${nodeKey}:${user}:blind_factor`);
+  await serverBroadcast(endpoints, `node-${index}:${user}:D_i`, Di);
+  await serverBroadcast(endpoints, `node-${index}:${user}:D_i_blind`, Diblind);
+  res.sendStatus(200);
+});
+
+app.post("/round_4_Di_verify", async (req, res) => {
+  let { user, index, parties } = req.body;
+  let b_proofs = [];
+  let blind_factors = [];
+  let g_gamma_is = [];
+  let coms = [];
+  for (let i = 0; i < parties.length; i++) {
+    let party = parties[i];
+    // note: these are length of n, other arrays are length n - 1
+    g_gamma_is.push(await db.get(`node-${party}:${user}:D_i`));
+    coms.push(await db.get(`node-${party}:${user}:com`));
+    blind_factors.push(await db.get(`node-${party}:${user}:D_i_blind`));
+    if (party === index) {
+      continue;
+    }
+    let m_b_gamma = await db.get(`user-${user}:from-${party}:to-${index}:m_b_gamma`);
+    b_proofs.push(tss.get_b_proof(m_b_gamma))
+  }
 
 
+  let deltaInv = await db.get(`${nodeKey}:${user}:delta_inv`);
+  console.log({
+    coms,
+    g_gamma_is,
+    blind_factors,
+    b_proofs,
+    deltaInv,
+    index,
+    parties
+  });
+  let R = tss.phase_4_Di_verify(
+    coms,
+    g_gamma_is,
+    blind_factors,
+    b_proofs,
+    deltaInv,
+    index,
+    parties
+  );
+  await db.set(`${nodeKey}:${user}:R`, R);
+  res.sendStatus(200);
+});
 
 (async () => {
   console.log("app listening on port", port);
