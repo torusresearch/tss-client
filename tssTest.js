b5f8c1e2a25bc7216a32d26faaf53f14a13a6417
Leonard <tetratorus@gmail.com>
2022-06-06 15:31:33 +0800

    tss test use log

--- tssTest.js
+++ tssTest.js
@@ -6,17 +6,18 @@ var axios = require("axios");
 var BN = require("bn.js");
 const { assert } = require("elliptic/lib/elliptic/utils");
 
-let onlinephasestart = process.hrtime();
+// let onlinephasestart = process.hrtime();
 
 var base_port = 8000;
 var endpoint_prefix = "http://localhost:";
 var msg = "hello world";
 var msgHash = keccak256(msg);
 var user = "test";
-var parties = [1,2,3,4,5,6];
-var endpoints = parties
-  .map((party) => `${endpoint_prefix}${base_port + parseInt(party)}`)
-const delay = 150
+var parties = [1, 2, 3, 4, 5, 6];
+var endpoints = parties.map(
+  (party) => `${endpoint_prefix}${base_port + parseInt(party)}`
+);
+const delay = 0;
 
 var privKey = new BN(eccrypto.generatePrivate());
 
@@ -38,7 +39,9 @@ assert.equal(reduced.toString(16), privKey.toString(16));
 (async () => {
   try {
     // write shares to each node
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+    let now = Date.now();
+    console.log("writing shares to nodes, started at", now);
     var awaiting = [];
     for (let i = 0; i < 6; i++) {
       let share = shares[i];
@@ -53,24 +56,26 @@ assert.equal(reduced.toString(16), privKey.toString(16));
       );
     }
     var gwis = await Promise.all(awaiting);
-    gwis = gwis.map(obj => obj.commitment);
+    gwis = gwis.map((obj) => obj.commitment);
     // console.log("gwis", gwis)
 
     // get node h1h2ntilde
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+    console.log("getting h1h2Ntildes", Date.now() - now);
     awaiting = [];
     for (let i = 1; i <= 6; i++) {
       let endpoint = `${endpoint_prefix}${base_port + i}`;
       awaiting.push(
-        axios.get(`${endpoint}/get_h1h2Ntilde`).then(res => res.data)
+        axios.get(`${endpoint}/get_h1h2Ntilde`).then((res) => res.data)
       );
     }
     var h1h2Ntildes = await Promise.all(awaiting);
-    h1h2Ntildes = h1h2Ntildes.map(obj => obj.h1h2Ntilde)
+    h1h2Ntildes = h1h2Ntildes.map((obj) => obj.h1h2Ntilde);
     // console.log("h1h2ntildes", h1h2Ntildes)
 
     // get node paillier eks
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+    console.log("getting eks", Date.now() - now);
     awaiting = [];
     for (let i = 1; i <= 6; i++) {
       let endpoint = `${endpoint_prefix}${base_port + i}`;
@@ -79,11 +84,12 @@ assert.equal(reduced.toString(16), privKey.toString(16));
       );
     }
     var eks = await Promise.all(awaiting);
-    eks = eks.map(obj => obj.ek)
+    eks = eks.map((obj) => obj.ek);
     // console.log("eks", eks)
 
     // publish pub key
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+    console.log("publish pubkey", Date.now() - now);
     awaiting = [];
     var pubkey = ec.curve.g.mul(privKey);
     for (let i = 1; i <= 6; i++) {
@@ -98,7 +104,8 @@ assert.equal(reduced.toString(16), privKey.toString(16));
     await Promise.all(awaiting);
 
     // round 1
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+    console.log("round 1", Date.now() - now);
     awaiting = [];
     for (let i = 1; i <= 6; i++) {
       let endpoint = `${endpoint_prefix}${base_port + i}`;
@@ -114,7 +121,8 @@ assert.equal(reduced.toString(16), privKey.toString(16));
     await Promise.all(awaiting);
 
     // round 2 MessageA
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+    console.log("round 2 message A", Date.now() - now);
     awaiting = [];
     for (let i = 1; i <= 6; i++) {
       let endpoint = `${endpoint_prefix}${base_port + i}`;
@@ -131,7 +139,9 @@ assert.equal(reduced.toString(16), privKey.toString(16));
     await Promise.all(awaiting);
 
     // round 2 MessageBs
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+
+    console.log("round 2 message Bs", Date.now() - now);
     awaiting = [];
 
     for (let i = 1; i <= 6; i++) {
@@ -144,13 +154,15 @@ assert.equal(reduced.toString(16), privKey.toString(16));
           endpoints,
           h1h2Ntildes,
           eks,
-        })
+        }).then(console.log)
       );
     }
     await Promise.all(awaiting);
 
     // round 2 Alphas
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+
+    console.log("round 2 message Alphas", Date.now() - now);
     awaiting = [];
     for (let i = 1; i <= 6; i++) {
       let endpoint = `${endpoint_prefix}${base_port + i}`;
@@ -160,14 +172,16 @@ assert.equal(reduced.toString(16), privKey.toString(16));
           user,
           parties,
           endpoints,
-          gwis
+          gwis,
         })
       );
     }
     await Promise.all(awaiting);
 
     // round 3 DeltaInv
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+
+    console.log("round 3 delta inv", Date.now() - now);
     awaiting = [];
     for (let i = 1; i <= 6; i++) {
       let endpoint = `${endpoint_prefix}${base_port + i}`;
@@ -215,7 +229,10 @@ assert.equal(reduced.toString(16), privKey.toString(16));
     // await Promise.all(awaiting);
 
     // round 4 Di
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+
+    console.log("round 4 Di", Date.now() - now);
+
     awaiting = [];
     for (let i = 1; i <= 6; i++) {
       let endpoint = `${endpoint_prefix}${base_port + i}`;
@@ -231,7 +248,10 @@ assert.equal(reduced.toString(16), privKey.toString(16));
     await Promise.all(awaiting);
 
     // round 4 Di_verify
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+
+    console.log("round 4 Di verify", Date.now() - now);
+
     awaiting = [];
     for (let i = 1; i <= 6; i++) {
       let endpoint = `${endpoint_prefix}${base_port + i}`;
@@ -247,7 +267,10 @@ assert.equal(reduced.toString(16), privKey.toString(16));
     await Promise.all(awaiting);
 
     // round 5 Rki
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+
+    console.log("round 5 Rki", Date.now() - now);
+
     awaiting = [];
     for (let i = 1; i <= 6; i++) {
       let endpoint = `${endpoint_prefix}${base_port + i}`;
@@ -264,7 +287,10 @@ assert.equal(reduced.toString(16), privKey.toString(16));
     await Promise.all(awaiting);
 
     // round 5 verify
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+
+    console.log("round 5 verify", Date.now() - now);
+
     awaiting = [];
     for (let i = 1; i <= 6; i++) {
       let endpoint = `${endpoint_prefix}${base_port + i}`;
@@ -281,7 +307,10 @@ assert.equal(reduced.toString(16), privKey.toString(16));
     await Promise.all(awaiting);
 
     // round 6 Rsigmai
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+
+    console.log("round 6 Rsigmai", Date.now() - now);
+
     awaiting = [];
     for (let i = 1; i <= 6; i++) {
       let endpoint = `${endpoint_prefix}${base_port + i}`;
@@ -297,7 +326,10 @@ assert.equal(reduced.toString(16), privKey.toString(16));
     await Promise.all(awaiting);
 
     // round 6 verify
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // await new Promise((resolve) => setTimeout(resolve, delay));
+
+    console.log("round 6 verify", Date.now() - now);
+
     awaiting = [];
     for (let i = 1; i <= 6; i++) {
       let endpoint = `${endpoint_prefix}${base_port + i}`;
@@ -312,17 +344,23 @@ assert.equal(reduced.toString(16), privKey.toString(16));
     }
     await Promise.all(awaiting);
 
-    let onlinephaseend = process.hrtime(onlinephasestart);
+    let online_phase = Date.now() - now
+    console.log(`Time taken for online phase: ${online_phase / 1e3 } seconds`)
 
-    console.log(
-      `Time taken for online phase: ${
-        (onlinephaseend[0] * 1e9 + onlinephaseend[1]) / 1e9
-      } seconds`
-    );
+    // let onlinephaseend = process.hrtime(onlinephasestart);
 
-    let offlinephasestart = process.hrtime();
+    // console.log(
+    //   `Time taken for online phase: ${
+    //     (onlinephaseend[0] * 1e9 + onlinephaseend[1]) / 1e9
+    //   } seconds`
+    // );
+
+    // let offlinephasestart = process.hrtime();
 
     // round 7
+
+    console.log("round 7")
+
     var s_is = [];
     for (let i = 1; i <= 6; i++) {
       let endpoint = `${endpoint_prefix}${base_port + i}`;
@@ -337,17 +375,17 @@ assert.equal(reduced.toString(16), privKey.toString(16));
       s_is.push(resp.data);
     }
 
-    s_is = s_is.map(obj => obj.s_i)
+    s_is = s_is.map((obj) => obj.s_i);
 
     // get signature
 
     let endpoint = `${endpoint_prefix}${base_port + 1}`;
-    var resp = await axios.post(
-      `${endpoint}/get_signature`,
-      {s_is, user, msg_hash: new BN(msgHash).toString("hex")}
-    );
+    var resp = await axios.post(`${endpoint}/get_signature`, {
+      s_is,
+      user,
+      msg_hash: new BN(msgHash).toString("hex"),
+    });
     var sig = JSON.parse(resp.data.sig);
-    
 
     let hexToDecimal = (x) =>
       ec.keyFromPrivate(x, "hex").getPrivate().toString(10);
@@ -364,13 +402,16 @@ assert.equal(reduced.toString(16), privKey.toString(16));
     let passed = ec.verify(msgHash, signature, pubk);
 
     console.log("passed: ", passed);
-    let offlinephaseend = process.hrtime(offlinephasestart);
 
-    console.log(
-      `Time taken for offline phase: ${
-        (offlinephaseend[0] * 1e9 + offlinephaseend[1]) / 1e9
-      } seconds`
-    );
+    console.log(`Time taken for offline phase: ${(Date.now() - now - online_phase) / 1e3} seconds`)
+
+    // let offlinephaseend = process.hrtime(offlinephasestart);
+
+    // console.log(
+    //   `Time taken for offline phase: ${
+    //     (offlinephaseend[0] * 1e9 + offlinephaseend[1]) / 1e9
+    //   } seconds`
+    // );
   } catch (e) {
     console.error(e);
   }
