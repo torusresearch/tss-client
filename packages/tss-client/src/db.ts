4ca952e5f09a0c764ba47a7dcad989db7aa9d072
ieow <cwlee0@gmail.com>
2023-02-08 11:02:17 +0800

    fix: bug in chrome get and set method

--- packages/tss-client/src/db.ts
+++ packages/tss-client/src/db.ts
@@ -3,7 +3,7 @@ import { DB } from "./types";
 class WebDb implements DB {
   get = (key: string): Promise<string> => {
     return new Promise((r) => {
-      chrome.storage.local.get(key).then((result) => {
+      chrome.storage.local.get([key]).then((result) => {
         r(result[key] as string);
         return result;
       });
@@ -11,12 +11,8 @@ class WebDb implements DB {
   };
 
   set = (key: string, value: string): Promise<void> => {
-    return chrome.storage.local.set({ key: value });
-    // return new Promise((r) => {
-    //   chrome.storage.local.set({ key: value });
+    return chrome.storage.local.set({ [key]: value });
     //   // memoryDB[key] = value
-    //   r();
-    // });
   };
 }
 
