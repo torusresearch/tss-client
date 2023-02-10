ae9bba8fc605f2769dbfcf77912f7eb143ec3c62
ieow <cwlee0@gmail.com>
2023-02-10 13:10:13 +0800

    callback required for chrome extension manifest v2

--- packages/tss-client/src/db.ts
+++ packages/tss-client/src/db.ts
@@ -3,7 +3,11 @@ import { DB } from "./types";
 class WebDb implements DB {
   get = (key: string): Promise<string> => {
     return new Promise((r) => {
-      chrome.storage.local.get([key]).then((result) => {
+      // chrome.storage.local.get([key]).then((result) => {
+      //   r(result[key] as string);
+      //   return result;
+      // });
+      chrome.storage.local.get(key, (result) => {
         r(result[key] as string);
         return result;
       });
@@ -11,7 +15,11 @@ class WebDb implements DB {
   };
 
   set = (key: string, value: string): Promise<void> => {
-    return chrome.storage.local.set({ [key]: value });
+    return new Promise((r) => {
+      chrome.storage.local.set({ [key]: value }, () => {
+        r();
+      });
+    });
     //   // memoryDB[key] = value
   };
 }
