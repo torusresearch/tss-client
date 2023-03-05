b96953bd62ae5cc486c1a58a4f6ed8349d12a514
Leonard Tan <lentan1029@gmail.com>
2023-03-05 22:05:28 +0800

    update deps for utils

--- packages/tss-client/src/index.ts
+++ packages/tss-client/src/index.ts
@@ -1,4 +1,5 @@
 import { Client } from "./client";
 import { localStorageDB } from "./db";
+import * as utils from "./utils";
 
-export { Client, localStorageDB };
+export { Client, localStorageDB, utils };
---
+++ packages/tss-client/src/utils.ts
@@ -0,0 +1,151 @@
+import BN from "bn.js";
+import { ec as EC } from "elliptic";
+import { io, Socket } from "socket.io-client";
+
+export const torusNodeEndpoints = [
+  "https://sapphire-dev-2-1.authnetwork.dev/sss/jrpc",
+  "https://sapphire-dev-2-2.authnetwork.dev/sss/jrpc",
+  "https://sapphire-dev-2-3.authnetwork.dev/sss/jrpc",
+  "https://sapphire-dev-2-4.authnetwork.dev/sss/jrpc",
+  "https://sapphire-dev-2-5.authnetwork.dev/sss/jrpc",
+];
+
+export function getEc(): any {
+  return new EC("secp256k1");
+}
+
+export function getLagrangeCoeffs(_allIndexes: number[] | BN[], _myIndex: number | BN, _target: number | BN = 0): BN {
+  const ec = getEc();
+  const allIndexes: BN[] = _allIndexes.map((i) => new BN(i));
+  const myIndex: BN = new BN(_myIndex);
+  const target: BN = new BN(_target);
+  let upper = new BN(1);
+  let lower = new BN(1);
+  for (let j = 0; j < allIndexes.length; j += 1) {
+    if (myIndex.cmp(allIndexes[j]) !== 0) {
+      let tempUpper = target.sub(allIndexes[j]);
+      tempUpper = tempUpper.umod(ec.curve.n);
+      upper = upper.mul(tempUpper);
+      upper = upper.umod(ec.curve.n);
+      let tempLower = myIndex.sub(allIndexes[j]);
+      tempLower = tempLower.umod(ec.curve.n);
+      lower = lower.mul(tempLower).umod(ec.curve.n);
+    }
+  }
+  return upper.mul(lower.invm(ec.curve.n)).umod(ec.curve.n);
+}
+
+export function ecPoint(p: { x: string; y: string }): any {
+  const ec = getEc();
+  return ec.keyFromPublic({ x: p.x.padStart(64, "0"), y: p.y.padStart(64, "0") }).getPublic();
+}
+
+export const getAdditiveCoeff = (isUser: boolean, participatingServerIndexes: number[], userTSSIndex: number, serverIndex?: number): BN => {
+  const ec = getEc();
+  if (isUser) {
+    return getLagrangeCoeffs([1, userTSSIndex], userTSSIndex);
+  }
+  // generate the lagrange coeff that converts the current server DKG share into an additive sharing
+  const serverLagrangeCoeff = getLagrangeCoeffs(participatingServerIndexes, serverIndex!);
+  const masterLagrangeCoeff = getLagrangeCoeffs([1, userTSSIndex], 1);
+  const additiveLagrangeCoeff = serverLagrangeCoeff.mul(masterLagrangeCoeff).umod(ec.curve.n);
+  return additiveLagrangeCoeff;
+};
+
+// Note: this is only needed for DKLS and not for FROST
+export const getDenormaliseCoeff = (party: number, parties: number[]): BN => {
+  if (parties.indexOf(party) === -1) throw new Error(`party ${party} not found in parties ${parties}`);
+  const ec = getEc();
+  // generate the lagrange coeff that denormalises the additive sharing into the shamir sharing that TSS is expecting
+  const denormaliseLagrangeCoeff = getLagrangeCoeffs(parties, party).invm(ec.curve.n).umod(ec.curve.n);
+  return denormaliseLagrangeCoeff;
+};
+
+export const getDKLSCoeff = (isUser: boolean, participatingServerIndexes: number[], userTSSIndex: number, serverIndex?: number): BN => {
+  const sortedServerIndexes = participatingServerIndexes.sort((a, b) => a - b);
+  for (let i = 0; i < sortedServerIndexes.length; i++) {
+    if (sortedServerIndexes[i] !== participatingServerIndexes[i]) throw new Error("server indexes must be sorted");
+  }
+  // generate denormalise coeff for DKLS
+  const parties = [];
+
+  // total number of parties for DKLS = total number of servers + 1 (user is the last party)
+  // server party indexes
+  let serverPartyIndex = 0;
+  for (let i = 0; i < participatingServerIndexes.length; i++) {
+    const currentPartyIndex = i + 1;
+    parties.push(currentPartyIndex);
+    if (participatingServerIndexes[i] === serverIndex) serverPartyIndex = currentPartyIndex;
+  }
+  const userPartyIndex = parties.length + 1;
+  parties.push(userPartyIndex); // user party index
+  if (isUser) {
+    const additiveCoeff = getAdditiveCoeff(isUser, participatingServerIndexes, userTSSIndex, serverIndex);
+    const denormaliseCoeff = getDenormaliseCoeff(userPartyIndex, parties);
+    const ec = getEc();
+    return denormaliseCoeff.mul(additiveCoeff).umod(ec.curve.n);
+  }
+  const additiveCoeff = getAdditiveCoeff(isUser, participatingServerIndexes, userTSSIndex, serverIndex);
+  const denormaliseCoeff = getDenormaliseCoeff(serverPartyIndex, parties);
+  const ec = getEc();
+  const coeff = denormaliseCoeff.mul(additiveCoeff).umod(ec.curve.n);
+  return coeff;
+};
+
+export const createSockets = async (wsEndpoints: string[]): Promise<Socket[]> => {
+  return wsEndpoints.map((wsEndpoint) => {
+    if (wsEndpoint === null || wsEndpoint === undefined) {
+      return null as any;
+    }
+    return io(wsEndpoint, {
+      path: "/tss/socket.io",
+      transports: ["websocket", "polling"],
+      withCredentials: true,
+      reconnectionDelayMax: 10000,
+      reconnectionAttempts: 10,
+    });
+  });
+};
+
+type key = { x: string; y: string };
+
+export function getTSSPubKey(dkgPubKey: any, userSharePubKey: key, userTSSIndex: number): any {
+  const serverLagrangeCoeff = getLagrangeCoeffs([1, userTSSIndex], 1);
+  const userLagrangeCoeff = getLagrangeCoeffs([1, userTSSIndex], userTSSIndex);
+  const serverTerm = ecPoint(dkgPubKey).mul(serverLagrangeCoeff);
+  const userTerm = ecPoint(userSharePubKey).mul(userLagrangeCoeff);
+  return serverTerm.add(userTerm);
+}
+
+export const generateEndpoints = (parties: number, clientIndex: number) => {
+  const endpoints: string[] = [];
+  const tssWSEndpoints: string[] = [];
+  const partyIndexes: number[] = [];
+  for (let i = 0; i < parties; i++) {
+    partyIndexes.push(i);
+    if (i === clientIndex) {
+      endpoints.push(null as any);
+      tssWSEndpoints.push(null as any);
+    } else {
+      endpoints.push(`https://sapphire-dev-2-${i + 1}.authnetwork.dev/tss`);
+      tssWSEndpoints.push(`https://sapphire-dev-2-${i + 1}.authnetwork.dev`);
+    }
+  }
+  return { endpoints, tssWSEndpoints, partyIndexes };
+};
+
+export const setupSockets = async (tssWSEndpoints: string[]) => {
+  const sockets = await createSockets(tssWSEndpoints);
+  // wait for websockets to be connected
+  await new Promise((resolve) => {
+    const checkConnectionTimer = setInterval(() => {
+      for (let i = 0; i < sockets.length; i++) {
+        if (sockets[i] !== null && !sockets[i].connected) return;
+      }
+      clearInterval(checkConnectionTimer);
+      resolve(true);
+    }, 100);
+  });
+
+  return sockets;
+};
