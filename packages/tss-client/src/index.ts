2688b853c79b9e8039a4930db46e47ee53e5f679
Matthias Geihs <matthias@tor.us>
2024-05-12 21:13:28 +0800

    export keyType and lib

--- packages/tss-client/src/index.ts
+++ packages/tss-client/src/index.ts
@@ -1,5 +1,9 @@
+import type * as WasmLib from "./dkls";
+
 export * from "./client";
 export * from "./constants";
 export * from "./db";
 export * from "./interfaces";
 export * from "./utils";
+
+export { WasmLib };
--- packages/tss-lib/src/index.ts
+++ packages/tss-lib/src/index.ts
@@ -1,10 +1,16 @@
-import { initSync } from "./pkg";
+import * as lib from "./pkg";
 import wasmDataURL from "./pkg/dkls_bg.wasm";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const base64Data = (wasmDataURL as any).split("base64,")[1];
 const wasmBuffer = Uint8Array.from(atob(base64Data), (c) => c.charCodeAt(0));
 
-initSync(wasmBuffer);
+lib.initSync(wasmBuffer);
 
-export * from "./pkg";
+const tssLib = {
+  keyType: "secp256k1",
+  lib,
+};
+
+export { tssLib };
+export default lib;
